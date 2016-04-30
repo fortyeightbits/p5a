7201ae13292199bb2fa28118e3156590a7353e63
Joanne Lee <joanne@galapagos-01.cs.wisc.edu>
2016-04-30 01:08:13 -0500

    nyam nyam ily

--- fscheck.c
+++ fscheck.c
@@ -7,19 +7,33 @@
 #include <sys/mman.h>
 #include "fs.h"
 
+#define STDERR_FD 2
+
 // TODO: populate with different errors
 typedef enum errno{
-    error1,
-    error2,
-    error3,
-    error4
+	noimg,
+    badinode,
+    badinodeadd,
+    norootdir,
+    baddirformat,
+	parentdirmismatch,
+	addmarkedfree,
+	blknotused,
+	addused,
+	inodenotindir,
+	inodefree,
+	badrefcnt,
+	dirused
 }e_errno;
 
 int main (int argc, char *argv[]){
     e_errno errorflag;
 
 	int fd = open(argv[1], O_RDONLY);
-	assert (fd > -1);
+	if (fd < 0){
+		errorflag = noimg;
+		goto bad;
+	}
 	int rc;
 	struct stat sbuf;
 	int rc = fstat (fd, &sbuf);
@@ -31,20 +45,63 @@ int main (int argc, char *argv[]){
 	sb = (struct superblock *) (img_ptr + BSIZE);
 	
 	
-	/*for inodes
 	int i ;
 	struct dinode *dip = (struct dinode*) (img_ptr + 2*BSIZE);
 	for (i = 0; i < sb->ninodes; i++){
-		printf("%s\n", dip->type);
+		if (dip->type < 0 || dip->type > 3){
+			errorflag = badinode;
+			goto bad;
+		}
+		
+		//direct pointers
+		int j;
+		for (j = 0; j < NDIRECT; j++){
+			if (dip->addrs[j] > (sb->size * BPB){
+				errorflag = badinodeadd;
+				goto bad;
+			}
+		}
+		
+		//indirect pointers
+		
+		
 		dip++; //will increment by size of dip
 	}
-	*/
 	
 	return 0;
 
     bad:
-    // TODO: Handle errors.
-    printf("found an error! Error number: %d\n", errorflag);
-
+	char* error_message;
+	
+    switch(errorflag){
+		case noimg :
+			error_message = "image not found"; break;
+		case badinode : 
+			error_message = "bad inode"; break;
+		case badinodeadd : 
+			error_message = "bad address in inode"; break;
+		case norootdir : 
+			error_message = "root directory does not exist"; break;
+		case baddirformat : 
+			error_message = "directory not properly formatted"; break;
+		case parentdirmismatch : 
+			error_message = "parent directory mismatch"; break;
+		case addmarkedfree : 
+			error_message = "address used by inode but marked free in bitmap"; break;
+		case blknotused : 
+			error_message = "bitmap marks block in use but it is not in use"; break;
+		case addused : 
+			error_message = "address used more than once"; break;
+		case inodenotindir : 
+			error_message = "inode marked use but not found in a directory"; break;
+		case inodefree : 
+			error_message = "inode referred to in directory but marked free"; break;
+		case badrefcnt : 
+			error_message = "bad reference count for file"; break;
+		case dirused : 
+			error_message = "directory appears more than once in file system"; break;
+	}
+	
+	write(STDERR_FILENO, error_message, strlen(error_message));
     return 1;
 }
